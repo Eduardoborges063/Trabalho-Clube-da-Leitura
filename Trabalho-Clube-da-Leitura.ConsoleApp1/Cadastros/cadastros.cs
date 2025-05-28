67882c5f6e0f9eb4ccfa81f09698990c30229cfe
Eduardoborges063 <1eduardomacedo2525@gmail.com>
2025-05-27 21:52:15 -0300

    Finalizando o dia.Fizmais algumas alterações nas opções cadastros e nas telas, fazendo as bases do projeto para começar a fazer as partes mais chatinhas.

--- Trabalho-Clube-da-Leitura.ConsoleApp1/Cadastros/cadastros.cs
+++ Trabalho-Clube-da-Leitura.ConsoleApp1/Cadastros/cadastros.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Trabalho_Clube_da_Leitura.ConsoleApp1.Módulo_de_Caixas;
 
 namespace Trabalho_Clube_da_Leitura.ConsoleApp1.Cadastros
 {
@@ -26,11 +27,44 @@ namespace Trabalho_Clube_da_Leitura.ConsoleApp1.Cadastros
             Console.WriteLine();
             Console.WriteLine("Digite o nome do livro: ");
             Console.WriteLine();
-            Console.WriteLine("Digite o nome do autor: ");
-            Console.WriteLine();
             Console.WriteLine("Digite a quantidade de páginas: ");
             Console.WriteLine();
+            Console.WriteLine("Ano de publicação: ");
+            Console.WriteLine("Caixa(seleção obrigatória):");
+            Console.WriteLine("Opções:drama, comédia, ficção científica, fantasia, terror, suspense, faroeste, romance e aventura");
+
+            Console.WriteLine();
             
+        }
+        public void TelaCadastrarCaixa()
+        {
+            Console.WriteLine("vamos cadastrar uma caixa:");
+            Console.WriteLine();
+            Console.WriteLine("Digite o nome da caixa: ");
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de livros na caixa: ");
+            Console.WriteLine();
+            Console.WriteLine("Digite o tipo de caixa: ");
+            Console.WriteLine("Opções: drama, comédia, ficção científica, fantasia, terror, suspense, faroeste, romance e aventura");
+            Console.WriteLine();
+            Console.WriteLine("Digite a Etiqueta(texto único, máximo 50 caracteres): ");
+            Console.WriteLine();
+            Console.WriteLine("Digite o prazo máximo para empréstimo de suas revistas(maximo 6 meses): ");
+
+        }
+        public void CadastrarCaixa()
+        {
+            Console.Clear();
+            Caixas caixas = new Caixas();
+            Caixas OpcoesDeCaixa;
+            OpcoesDeCaixa = new Caixas();
+            string nomeCaixa;
+            int quantidadeLivros;
+            string tipoCaixa;
+            string etiqueta;
+            int prazoMaximoEmprestimo;
+
+
         }
     }
 }
---
+++ Trabalho-Clube-da-Leitura.ConsoleApp1/Módulo de Caixas/Caixas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Clube_da_Leitura.ConsoleApp1.Módulo_de_Caixas
+{
+    public class Caixas
+    {
+        public void OpcoesDeCaixa()
+        {
+           Console.Clear();
+            string drama;
+            string comedia;
+            string ficcaoCientifica;
+            string fantasia;
+            string terror;
+            string suspense;
+            string faroeste;
+            string romance;
+            string aventura;
+        }
+       
+    }
+}
